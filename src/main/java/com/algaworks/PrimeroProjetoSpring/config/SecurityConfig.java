5ae4739fe937aeccc2b8b47bbc9fdaa7546ed42a
iagolourinho <37443475+iagolourinho@users.noreply.github.com>
2021-01-22 15:41:40 -0300

    Finalizando tela login

--- src/main/java/com/algaworks/PrimeroProjetoSpring/config/SecurityConfig.java
+++ src/main/java/com/algaworks/PrimeroProjetoSpring/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -12,7 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-        .withUser("iago").password("iago").roles("");
+        .withUser("iago").password("iago").roles("CADASTRAR_VINHOS","LISTAR_VINHO")
+        .and()
+        .withUser("fatima").password("fatima").roles("CADASTRAR_VINHOS");
     }
 
     @Override
@@ -23,9 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+            .antMatchers("/vinhos/novo").hasRole("CADASTRAR_VINHOS")
+            .antMatchers("/vinhos/**").hasRole("LISTAR_VINHO")
             .anyRequest().authenticated()
             .and()
-            .formLogin();
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 
     }
     
--- src/main/resources/static/JavaScript/vinho.upload-foto.js
+++ src/main/resources/static/JavaScript/vinho.upload-foto.js
@@ -1,19 +1,45 @@
-$(function() {
-	var uploadDrop = $('#upload-drop');
-	var containerFoto = $('.js-container-foto');
+var wine = wine || {};
+
+wine.UploadFoto = (function(){
 	
-	var settings = {
-		type: 'json',
-		filelimit: 1,
-		allow: '*.(jpg|jpeg|png)',
-		action: '/fotos/' + uploadDrop.data('codigo'),
-		complete: function(foto) {
-			console.log('...resposta: ',foto.url);
-			uploadDrop.addClass('hidden');
-			containerFoto.prepend('<img src="' + foto.url + '" class="img-responsive" style="margin : auto"/>');
-		}
-	};
+	function UploadFoto(){
+	this.uploadDrop = $('#upload-drop');
+	this.containerFoto = $('.js-container-foto');
+	}
 	
-	UIkit.uploadSelect($('#upload-select'),settings);
-	UIkit.uploadDrop(uploadDrop,settings);
+	UploadFoto.prototype.iniciar = function() {
+		var settings = {
+			type: 'json',
+			filelimit: 1,
+			allow: '*.(jpg|jpeg|png)',
+			action: '/fotos/' + this.uploadDrop.data('codigo'),
+			complete: onUploadCompleto.bind(this),
+			beforeSend: adicionarCsrfToken
+				
+			
+		};
+		
+		UIkit.uploadSelect($('#upload-select'),settings);
+		UIkit.uploadDrop(this.uploadDrop,settings);
+	}
+
+	function adicionarCsrfToken(xhr){
+		var header = $('input[name=_csrf_header]').val();
+		var token =  $('input[name=_csrf]').val();
+		xhr.setRequestHeader(header,token);
+	}
+
+	function onUploadCompleto(foto){
+		console.log('...resposta: ',foto.url);
+		this.uploadDrop.addClass('hidden');
+		this.containerFoto.prepend('<img src="' + foto.url + '" class="img-responsive" style="margin : auto"/>');
+			
+	}
+
+	return UploadFoto;
+})();
+
+$(function(){
+	var UploadFoto = new wine.UploadFoto();
+	UploadFoto.iniciar();
 });
